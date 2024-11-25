931d001a0d79f996b068a4c08f9e0ea639050405
colayyun <koyun0902@gmail.com>
2024-11-25 16:28:48 +0800

    第三次commit

--- app/src/main/java/tw/edu/pu/s1110054/game2d/Game.kt
+++ app/src/main/java/tw/edu/pu/s1110054/game2d/Game.kt
@@ -4,11 +4,23 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import android.content.Context
+import android.media.MediaPlayer
 
-class Game(val scope: CoroutineScope, val screenW:Int, val screenH: Int) {
+
+
+class Game(val scope: CoroutineScope, val screenW:Int, val screenH: Int, scale:Float, context: Context) {
     var counter = 0
     val state = MutableStateFlow(0)
     val background = Background(screenW)
+    val boy = Boy(screenH, scale)
+    val virus = Virus(screenW, screenH, scale)
+
+    //背景音樂
+    var mper1 = MediaPlayer.create(context, R.raw.lastletter)
+    //結束音樂
+    var mper2 = MediaPlayer.create(context, R.raw.gameover)
+
 
 
     var isPlaying = true
@@ -19,12 +31,33 @@ class Game(val scope: CoroutineScope, val screenW:Int, val screenH: Int) {
             isPlaying = true
 
             while (isPlaying) {
-                delay(10)
+                mper1.start()  //播放背景音樂
+                delay(35)
                 background.Roll()
+                if (counter % 3 == 0){
+                    boy.Walk()
+                    virus.Fly()
+                    //判斷是否碰撞，結束遊戲
+                    if(boy.getRect().intersect(virus.getRect())) {
+                        isPlaying = false
+                        //遊戲結束音效
+                        mper1.pause()
+                        mper2.start()
+                    }
+                }
+
                 counter++
                 state.emit(counter)
 
             }
         }
     }
+
+    fun Restart(){
+        virus.Reset()
+        counter = 0
+        isPlaying = true
+        Play()
+    }
+
 }
